22fed5f8cd382daf2b77b0ce0853fe69732c202b
Fadly Kayo <fadly.kayo@googlemail.com>
2017-02-25 10:44:50 +0700

    WIP 1

---
+++ client/index.js
@@ -0,0 +1,82 @@
+$(document).ready(function () {
+  $('ul.tabs').tabs()
+})
+
+var app = new Vue({
+  el: '#root',
+  data: {
+    add: {
+      content: ''
+    },
+    edit: {
+      _id: '',
+      content: ''
+    },
+    games: []
+  },
+  methods: {
+    loadGames: function () {
+      $.ajax({
+        type: 'GET',
+        url: 'http://localhost:3000/api/games',
+        success: function (resp) {
+          for (let i = 0; i < resp.length; i++) {
+            app.games = resp[i].content
+          }
+        },
+        error: function () {
+          console.log('GET loadGames request error')
+        }
+      })
+    },
+
+    addGame: function () {
+      $.ajax({
+        type: 'POST',
+        url: 'http://localhost:3000/api/games',
+        data: {
+          content: app.add.content
+        },
+        success: function (resp) {
+          console.log(resp.content)
+          console.log('add ', [{content: resp.content}])
+          app.add.content = ''
+          app.games = [{content: resp.content}]
+        },
+        error: function () {
+          console.log('POST addGame request error')
+        }
+      })
+    },
+
+    getGame: function (id) {
+      $.ajax({
+        type: 'GET',
+        url: `http://localhost:3000/api/twatt/${id}`,
+        success: function (resp) {
+          app.edit._id = resp._id,
+          app.edit.content = resp.content,
+          app.edit.userid = resp.userid,
+          app.edit.postedby = resp.postedby,
+          app.edit.tag = resp.tag
+        },
+        error: function () {
+          console.log('GET getGame request error')
+        }
+      })
+    },
+
+    deleteGame: function (id) {
+      $.ajax({
+        type: 'DELETE',
+        url: `http://localhost:3000/api/twatt/${id}`,
+        success: function (resp) {
+          app.loadGames()
+        },
+        error: function () {
+          console.log('DELETE deleteGame request error')
+        }
+      })
+    }
+  }
+})
